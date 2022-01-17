2fa2df3df6cc1b4f0379c602b6d8a81c5a146b6d
Henoch0 <44028287+Henoch0@users.noreply.github.com>
2022-01-17 20:14:30 +0100

    Update TwoColContactUsWithIllustrationFullForm.js

--- src/components/forms/TwoColContactUsWithIllustrationFullForm.js
+++ src/components/forms/TwoColContactUsWithIllustrationFullForm.js
@@ -25,7 +25,7 @@ const TextContent = tw.div`lg:py-8 text-center md:text-left dark:text-gray-100 `
 const Subheading = tw(SubheadingBase)`text-center md:text-left`;
 const Heading = tw(SectionHeading)`mt-4 font-black text-left text-3xl sm:text-4xl lg:text-5xl text-center md:text-left leading-tight`;
 const Description = tw.p`mt-4 text-center md:text-left text-sm md:text-base lg:text-lg font-medium leading-relaxed text-secondary-100 dark:text-gray-300`
-
+const Text = tw.div`dark:text-gray-100`;
 const Form = tw.form`mt-8 md:mt-10 text-sm flex flex-col max-w-sm mx-auto md:mx-0 `
 const Input = tw.input`mt-6 first:mt-0 border-b-2 py-3 focus:outline-none font-medium transition duration-300 hocus:border-blue-500 dark:text-gray-900 dark:bg-gray-200`
 const Textarea = styled(Input).attrs({ as: "textarea" })`
@@ -60,7 +60,7 @@ export default ({
       }, (error) => {
         console.log(error.text);
       });
-      e.target.reset();
+    e.target.reset();
   }
   // The textOnLeft boolean prop can be used to display either the text on left or right side of the image.
 
@@ -80,6 +80,32 @@ export default ({
               <Input type="text" name="name" placeholder="Dein Name" />
               <Input type="text" name="subject" placeholder="Betreff" />
               <Textarea name="message" placeholder="Teile uns hier bitte ganz genau mit was du von uns benötigst :)" />
+
+             
+              <div className="mx-auto py-12 flex justify-center">
+            
+                <div className="flex flex-col items-start">
+                <p>Zustimmung Datenschutz:</p>
+                  <div className="py-4 flex items-center">
+                    <div className="bg-white dark:bg-gray-800 border rounded-sm border-gray-400 dark:border-gray-700 w-5 h-5 flex flex-shrink-0 justify-center items-center relative">
+                      <input defaultChecked type="checkbox" className="checkbox opacity-0 absolute cursor-pointer w-full h-full" />
+                      <div className="check-icon hidden bg-indigo-700 text-white rounded-sm">
+                        <svg className="icon icon-tabler icon-tabler-check" xmlns="http://www.w3.org/2000/svg" width={25} height={25} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" >
+                          <path stroke="none" d="M0 0h24v24H0z" />
+                          <path d="M5 12l5 5l10 -10" />
+                        </svg>
+                      </div>
+                    </div>
+                    <p className="ml-3 text-base leading-4 font-normal">Sie erklären sich damit einverstanden, dass Ihre Daten zur Bearbeitung Ihres Anliegens verwendet werden. Informationen und Widerrufshinweise finden Sie unten in der Datenschutzerklärung.</p>
+                  </div>
+                  <style>
+                    {`  .checkbox:checked + .check-icon {
+                            display: flex;
+                        }`}
+                  </style>
+                
+                </div>
+              </div>
               <SubmitButton type="submit">{submitButtonText}</SubmitButton>
             </Form>
           </TextContent>
